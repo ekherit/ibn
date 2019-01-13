cbf10054a3cbce740fd8d2471f9496c14f41520c
Ivan Nikolaev <I.B.Nikolaev@inp.nsk.su>
2019-01-13 13:40:26 +0700

    make_number alias

--- test/natural_number.cpp
+++ test/natural_number.cpp
@@ -59,7 +59,7 @@ struct Str
   using Nt = typename TailNumber<Number>::type;
   using str = Str<Nt>;
   public:
-  Str(int opt = 2)
+  Str(int opt = 0)
   {
     value += str(opt).value;
     switch(opt)
@@ -70,7 +70,8 @@ struct Str
         break;
       case 2: {
                 std::stringstream oss;
-                oss << std::setw(sizeof(D1::digit)*2) << std::hex << std::setfill('0') << D1::digit;
+                //oss << std::setw(sizeof(D1::digit)*2) << std::hex << std::setfill('0') << D1::digit;
+                oss << std::hex << D1::digit;
                 value+=oss.str();
               }
               break;
@@ -85,9 +86,7 @@ template<typename Number>
 std::ostream & operator<<(std::ostream & os, const Str<Number> & s)
 {
   using D1 = typename FirstDigit<Number>::type;
-  //os <<  std::setw(sizeof(D1)*8*Number::size)  << s.value;
   os << s.value;
-  //os << std::setw(sizeof(typename D1::type)*2*Number::size) << s.value;
   return os;
 };
 
@@ -96,10 +95,10 @@ struct Nx2
 {
   using N2 = typename Add<N,N>::type;
   using N21 = typename Add<NaturalNumber<Digit<1>>, N2>::type;
-  static void print(int opt, int width,char c)
+  static void print(int opt=0, int width=0,char c=' ')
   {
     std::cout << std::setw(width) << std::setfill(c) << Str<N>(opt) << std::endl;
-    Nx2<n-1,N21>::print(opt,width,c);
+    Nx2<n-1,N2>::print(opt,width,c);
   };
 };
 
@@ -112,13 +111,29 @@ struct Nx2<0, N>
   };
 };
 
+template<int n, typename N=NaturalNumber<Digit<1>>>
+struct Inc
+{
+  using N21 = typename Add<NaturalNumber<Digit<1>>, N>::type;
+  static void print(int opt=0, int width=0,char c=' ')
+  {
+    std::cout << std::setw(width) << std::setfill(c) << Str<N>(opt) << std::endl;
+    Inc<n-1,N21>::print(opt,width,c);
+  };
+};
 
-
-
+template<typename N>
+struct Inc<0, N>
+{
+  static void print(int opt, int width, char c )
+  {
+    std::cout << std::setw(width) << std::setfill(c) << Str<N>(opt) << std::endl;
+  };
+};
 
 int main()
 {
-  using n1 = NaturalNumber<Digit<4'135'146'346>>;
+  using n1 = NaturalNumber<Digit<1>>;
   using n2 = NaturalNumber<Digit<4'105'146'345>>;
   //using n1 = NaturalNumber<Digit<122>>;
   //using n2 = NaturalNumber<Digit<100>>;
@@ -134,9 +149,26 @@ int main()
   using n6 = NaturalNumber<Digit<5>,Digit<6>, Digit<7>, Digit<8>, Digit<9>, Digit<10>>;
   using sum9 = typename Add<n4,n6>::type;
   std::cout << "sum9=" << sum9() <<std::endl;
-  static const int N=512;
-  Nx2<N-1, NaturalNumber<Digit<1>>>::print(2,N/4,'0');
+  static const int N=128;
+  Nx2<N-1, NaturalNumber<Digit<1>>>::print();
   //std::cout << Str<sum9>() << std::endl;
+  using n9 = NaturalNumber<Digit<9>>;
+  using n10 = typename Add< n9, n1>::type;
+  std::cout << Str<n10>(0) << std::endl;
+  using b1 = NaturalNumber<Digit<1,2-1>>;
+  using o1 = NaturalNumber<Digit<1,8-1>>;
+  Nx2<20, o1>::print();
+  //Inc<800>::print();
+
+  using D8 = make_number<8>;
+  std::cout << Str<D8>() << std::endl;
+  using H256 = make_number<256, 15>;
+  std::cout << "H256=" << Str<H256>() << std::endl;
+  using H255 = make_number<255, 15>;
+  std::cout << "H255=" << Str<make_number<65535,15>>(2) << std::endl;
+  using O8 = make_number<8,7>;
+  std::cout << "O8=" << Str<O8>() << std::endl;
+  using r = RealNumber<n9, n10>;
 };
 
 
